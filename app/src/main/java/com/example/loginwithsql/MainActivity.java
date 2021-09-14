3a0c3b351941c2fe15031a0c79fd4c4944d90c60
Musfiq <mushfiqur15-10872@diu.edu.bd>
2021-09-14 20:57:08 +0600

    Working on SignIn and SignUp Activity

--- app/src/main/java/com/example/loginwithsql/MainActivity.java
+++ app/src/main/java/com/example/loginwithsql/MainActivity.java
@@ -2,20 +2,47 @@ package com.example.loginwithsql;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper databaseHelper;
+    private EditText username, pass;
+    private Button singIn, signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        username = findViewById(R.id.userEditId);
+        pass = findViewById(R.id.passEditId);
+        singIn = findViewById(R.id.signInBtn);
+        signUp = findViewById(R.id.signUpBtn);
+
         databaseHelper = new DatabaseHelper(this);
         SQLiteDatabase sqLiteDatabase = databaseHelper.getWritableDatabase();
+
+        singIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = username.getText().toString();
+                String password = pass.getText().toString();
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
